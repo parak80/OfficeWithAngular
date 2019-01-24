912eb0ee6a374cd9f42573426d7fb29c3a317d73
parisaCarl <parisa.carlborg@gmail.com>
2019-01-24 17:10:04 +0100

    css changes

--- src/app/services/fake-backend.service.ts
+++ src/app/services/fake-backend.service.ts
@@ -3,14 +3,14 @@ import {InMemoryDbService} from 'angular-in-memory-web-api';
 export class FakeBackendService implements InMemoryDbService {
     createDb() {
         const tasks = [
-            { id: 1, documentId: 'KS2014/42', name: 'search-result 1' },
-            { id: 2, documentId: 'KS2014/42', name: 'search result 2' },
-            { id: 3, documentId: 'KS2014/42', name: 'search result 3' },
-            { id: 4, documentId: 'KS2014/42', name: 'search result 4' },
-            { id: 5, documentId: 'KS2014/42', name: 'search-result 5' },
-            { id: 6, documentId: 'KS2014/42', name: 'search result 6' },
-            { id: 7, documentId: 'KS2014/42', name: 'search result 7' },
-            { id: 8, documentId: 'KS2014/42', name: 'search result 8' }
+            { id: 1, documentId: 'KS 2014/42', name: 'search-result 1' },
+            { id: 2, documentId: 'KS 2014/42', name: 'search result 2' },
+            { id: 3, documentId: 'KS 2014/42', name: 'search result 3' },
+            { id: 4, documentId: 'KS 2014/42', name: 'search result 4' },
+            { id: 5, documentId: 'KS 2014/42', name: 'search-result 5' },
+            { id: 6, documentId: 'KS 2014/42', name: 'search result 6' },
+            { id: 7, documentId: 'KS 2014/42', name: 'search result 7' },
+            { id: 8, documentId: 'KS 2014/42', name: 'search result 8' }
         ];
         return {tasks};
     }
