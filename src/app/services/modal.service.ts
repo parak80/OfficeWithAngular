a153327e2ddf03524391d18c744a3fd3a22f0cc5
parisaCarl <parisa.carlborg@gmail.com>
2019-01-30 15:20:44 +0100

    fixing dialog

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -3,11 +3,11 @@ import {  } from '@angular/core';
 export class ModalService {
   private modals: any[] = [];
 
-  open(id: string) {
+  open(id: number) {
     const modal: any = this.modals.filter(x => x.id === id)[0];
     modal.open();
   }
-  close(id: string) {
+  close(id: number) {
     const modal: any = this.modals.filter(x => x.id === id)[0];
     modal.close();
   }
